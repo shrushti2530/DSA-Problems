bc401b385d1a0a2ed15c3be8a59a81511f049648
Shrushti Thigale <59228754+shrushti2530@users.noreply.github.com>
2025-01-02 15:06:48 +0530

    Update createqueue.js

--- createqueue.js
+++ createqueue.js
@@ -4,26 +4,36 @@
       let currentSize = queue.length;
       let maxSize = 5;
 
-      function enqueue(newVal) {
-        if (currentSize >= maxSize) {
+      function enqueue(newVal) 
+      {
+        if (currentSize >= maxSize) 
+        {
           alert("Queue is already full");
-        } else {
+        } else 
+        {
           queue[currentSize] = newVal;
           currentSize++;
         }
       }
 
-      function display() {
+      function display() 
+      {
+          
         console.warn(queue);
       }
-      function dequeue() {
-        if (currentSize > 0) {
-          for (let i = 0; i < queue.length; i++) {
+      function dequeue() 
+      {
+        if (currentSize > 0) 
+        {
+          for (let i = 0; i < queue.length; i++) 
+          {
             queue[i] = queue[i + 1];
           }
           currentSize--;
           queue.length = currentSize;
-        } else {
+        } 
+        else 
+        {
           alert("queue is already empty");
         }
       }
