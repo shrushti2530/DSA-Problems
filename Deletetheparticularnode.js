13dab12b94d2bd6025c6d2c23883d5736fc6fdde
Shrushti Thigale <59228754+shrushti2530@users.noreply.github.com>
2025-01-02 14:05:52 +0530

    Update Deletetheparticularnode.js

--- Deletetheparticularnode.js
+++ Deletetheparticularnode.js
@@ -6,21 +6,21 @@ class ListNode {
 }
 
 function deleteNode(head, valueToDelete) {
-    if (!head) return null; // If the list is empty, return null
+    if (!head) return null; 
 
-    // If the node to delete is the head
+   
     if (head.value === valueToDelete) {
         return head.next; // Return the next node as the new head
     }
 
     let current = head;
 
-    // Traverse the list to find the node before the one to delete
+   
     while (current.next && current.next.value !== valueToDelete) {
         current = current.next;
     }
 
-    // If the node was found, skip it in the list
+   
     if (current.next) {
         current.next = current.next.next;
     }
